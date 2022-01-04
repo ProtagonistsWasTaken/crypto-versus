f57643dd93b868906028aeba0b1f572fd5085e18
ThePywon <79721537+ThePywon@users.noreply.github.com>
2022-01-04 14:02:07 -0500

    Create index.js

---
+++ paths/index.js
@@ -0,0 +1,8 @@
+// basically precompiled headers but javascript. import this to get all routes
+module.exports = [
+  require("./main.js"), 
+  require("./signup.js"), 
+  require("./login.js"), 
+  require("./dostuff.js"),
+  require("./refresh.js")
+];
