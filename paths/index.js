41285e1d0ecc5a4f75fd4c22ad78e36a8b4df2d2
Pywon <79721537+ThePywon@users.noreply.github.com>
2022-01-17 12:21:28 +0000

    small tweaks + front end

--- paths/index.js
+++ paths/index.js
@@ -1,12 +1,12 @@
 // basically precompiled headers but javascript. import this to get import all routes
 module.exports = [
-  require("./main.js"), 
-  require("./account/signup.js"), 
-  require("./account/login.js"), 
-  require("./account/edit.js"),
-  require("./account/delete.js"),
-  require("./dostuff.js"),
-  require("./token/refresh.js"),
-  require("./account/key.js"),
+  require("./main"), 
+  require("./account/signup"), 
+  require("./account/login"), 
+  require("./account/edit"),
+  require("./account/delete"),
+  require("./dostuff"),
+  require("./token/refresh"),
+  require("./account/key"),
   require("./securitytxt")
 ];
