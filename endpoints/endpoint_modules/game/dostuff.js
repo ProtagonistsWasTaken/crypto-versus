4ac23e63fe5bbfd219eade2602fb80cb1f81ac43
ThePywon <79721537+ThePywon@users.noreply.github.com>
2022-02-16 17:35:08 -0500

    Update dostuff.js

--- endpoints/endpoint_modules/game/dostuff.js
+++ endpoints/endpoint_modules/game/dostuff.js
@@ -1,10 +1,38 @@
-const { Token } = require("../../../miscellaneous/token_handler.js");
+const { Token, sendError, Webhook } = require("../../../miscellaneous");
+const { User } = require("../../../database/schemas.js");
 
 module.exports = {
   urls:["api/dostuff"],
-  run:async function(req, res, data) {
-    var token = Token.fromString(res, data.token);
-    if(token) res.end(`Successfully did stuff as ${token.user}`);
+  run:function(req, res, data) {
+    return new Promise((resolve, reject)=>{
+      const token = Token.fromString(res, data.token);
+      if(token) {
+        User.findOne({username: token.user}).then(user=>{
+          if(!user.events.enabled)
+            resolve(sendError(res, {code:400,
+              message:"Events disabled",
+              body:`${user.username} does not have events enabled`
+            }));
+          else {
+            Webhook.send(user, {name:"ping",data:"You did stuff!"}).then(()=>{
+              res.end(`Successfully did stuff as ${token.user}`);
+              resolve();
+            }).catch(e=>{
+              if(e === "Timeout")
+                sendError(res, {code:408,
+                  body:`The host for ${user.username} did not respond in time`
+                });
+              else
+                sendError(res, {code:400,
+                  message:"Invalid host response",
+                  body:`The host for ${user.username} responded in an unexpected way`
+                });
+              resolve();
+            });
+          }
+        });
+      }
+    });
   },
   method:'POST'
 }
