0684f916d07f477baf7d3e4bf2061350005dc8d9
ThePywon <79721537+ThePywon@users.noreply.github.com>
2022-02-16 17:31:07 -0500

    Update index.js

--- miscellaneous/index.js
+++ miscellaneous/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require("./error"),
   ...require("./helper.js"),
-  ...require("./token_handler.js")
+  ...require("./token_handler.js"),
+  ...require("./webhook_handler.js")
 }
