beda70a6e4d08c2407e25d177626df0d043bee8c
ThePywon <ThePywon@hotmail.com>
2022-06-19 17:46:55 -0400

    Fixed up login.js

--- src/endpoints/endpoint_modules/account/login.js
+++ src/endpoints/endpoint_modules/account/login.js
@@ -1,74 +1,79 @@
 // this route handles all /login requests
 const bcrypt = require("bcrypt");
-const { Salt, User } = require("../../../database/schemas.js");
-const { Token, Tokens, validateUserInfo, sendError } = require("../../../miscellaneous");
+const { Salt, User } = require("/src/database/mongodbSchemas.js");
+const userSchema = require("/src/database/schemas.js");
+const { generateToken, validateUserInfo, sendError } = require("/src/miscellaneous");
 
 module.exports = {
-  urls:["api/login","api/signin","api/sign-in", "api/account/login", "api/account/signin", "api/account/sign-in"],
-  run:async function(req, res, data) {
-    let newToken;
-    let token;
-    let user;
-    let salt;
+  urls: ["api/login","api/signin","api/sign-in", "api/account/login", "api/account/signin", "api/account/sign-in"],
+  run: async function(req, res, data) {
 // Login using credentials
     if(data.username && data.password) {
       if(validateUserInfo(res, data)) {
 
-        salt = await Salt.find();
+        const salt = await Salt.find();
         const password = await bcrypt.hash(data.password, salt[0].val);
-        user = await User.findOne({username: data.username});
+        const user = await User.findOne({username: data.username});
 
-        if(user === null) sendError(res, {code:401,
-          message:"Account not found.",
-          body:`${data.username} doesn't exist.`
-        });
-        else if(user.password != password) sendError(res, {code:403,
-          message:"Login unsuccessful.",
-          body:"Invalid password."
-        });
+        if(user === null || user.password != password)
+          sendError(res, {
+            code: 403,
+            message: "Login unsuccessful.",
+            body: "Invalid credentials."
+          });
         else {
-          // Invalidate previous tokens (if any)
-          token = Tokens.findOne({user: data.username});
-          if(token !== null) token.invalidate();
           // Generate a token
-          newToken = new Token(data.username, 32, 600000);
-          res.setHeader("user", newToken.user);
-          res.setHeader("expire", newToken.lifetime);
-          res.setHeader("key", user.keyEnabled ? user.keyEnabled : false);
-          res.end(newToken.value);
+          const token = generateToken(32);
+
+          user.token.value = token;
+          user.token.expire = Date.now() + 1000 * 60 * 10;
+
+          await user.save();
+
+          res.setHeader("user", user.username);
+          res.setHeader("expire", user.token.expire);
+          res.setHeader("key", user.key.enabled);
+          res.end(token);
         }
       }
     }
     // Login using api key
     else if(data.key) {
-      salt = await Salt.find();
+      const salt = await Salt.find();
       const key = await bcrypt.hash(data.key, salt[0].val);
-      user = await User.findOne({key});
+      const user = await User.findOne({ key: { value: key } });
 
-      if(user === null) sendError(res, {code:401,
-        message:"Account not found.",
-        body:`${data.username} doesn't exists.`
-      });
-      else if(!user.keyEnabled) sendError(res, {code:403,
-        message:"Api key disabled.",
-        body:`${user.username} does not have key enabled.`
-      });
+      if(user === null)
+        sendError(res, {
+          code: 403,
+          message: "Login unsuccessful.",
+          body: "Invalid credentials."
+        });
+      else if(!user.key.enabled)
+        sendError(res, {
+          code: 403,
+          message: "Api key disabled.",
+          body: `Api key is disabled on this account.`
+        });
       else {
-        // Invalidate previous tokens (if any)
-        token = Tokens.findOne({user: user.username});
-        if(token !== null) token.invalidate();
-
         // Generate a token
-        newToken = new Token(user.username, 32, 600000);
-        res.setHeader("user", newToken.user);
-        res.setHeader("expire", newToken.lifetime);
-        res.setHeader("key", user.keyEnabled ? user.keyEnabled : false);
-        res.end(newToken.value);
+        const token = generateToken(32);
+
+        user.token.value = token;
+        user.token.expire = Date.now() + 1000 * 60 * 10;
+
+        await user.save();
+
+        res.setHeader("user", user.username);
+        res.setHeader("expire", user.token.expire);
+        res.setHeader("key", user.key.enabled);
+        res.end(token);
       }
     }
-    else sendError(res, {code:400,
-      message:"Login unsuccessful.",
-      body:"Missing login info."
+    else sendError(res, {
+      code: 400,
+      message: "Login unsuccessful.",
+      body: "Missing login info."
     });
   },
   method:'POST'
